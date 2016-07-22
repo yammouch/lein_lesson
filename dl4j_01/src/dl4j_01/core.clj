e7d1afc60eb6f958a7c2f9e8fa90ef219508a7ea
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-07-22 11:54:51 +0900

    'import's look to work well.

--- dl4j_01/src/dl4j_01/core.clj
+++ dl4j_01/src/dl4j_01/core.clj
@@ -1,6 +1,22 @@
 (ns dl4j-01.core)
 
-(defn foo
+(import '(org.deeplearning4j.eval Evaluation)
+        '(org.deeplearning4j.nn.api Layer OptimizationAlgorithm)
+        '(org.deeplearning4j.nn.conf MultiLayerConfiguration
+                                     NeuralNetConfiguration
+                                     NeuralNetConfiguration$ListBuilder)
+        '(org.deeplearning4j.nn.conf.distribution UniformDistribution)
+        '(org.deeplearning4j.nn.conf.layers DenseLayer OutputLayer
+                                            OutputLayer$Builder)
+        '(org.deeplearning4j.nn.multilayer MultiLayerNetwork)
+        '(org.deeplearning4j.nn.weights WeightInit)
+        '(org.deeplearning4j.optimize.listeners ScoreIterationListener)
+        '(org.nd4j.linalg.api.ndarray INDArray)
+        '(org.nd4j.linalg.dataset DataSet)
+        '(org.nd4j.linalg.factory Nd4j)
+        '(org.nd4j.linalg.lossfunctions LossFunctions))
+
+(defn -main
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
