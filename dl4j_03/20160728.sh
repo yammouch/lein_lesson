9aca244b8929aefb0bc23bb56e59160fdb471c61
yammouch <yammouch@gmail.com>
2016-07-28 16:10:49 +0900

    First checkin.

---
+++ dl4j_03/20160728.sh
@@ -0,0 +1,41 @@
+# 0.5 hour per line
+lein run 60000 4  100 | tee result/iter60000_4layers_100perlayer.log
+lein run 40000 4  150 | tee result/iter40000_4layers_150perlayer.log
+lein run 30000 4  200 | tee result/iter30000_4layers_200perlayer.log
+lein run 20000 4  300 | tee result/iter20000_4layers_300perlayer.log
+lein run 15000 4  400 | tee result/iter15000_4layers_400perlayer.log
+lein run 12500 4  480 | tee result/iter12500_4layers_480perlayer.log
+lein run 10000 4  600 | tee result/iter10000_4layers_600perlayer.log
+lein run  7500 4  800 | tee result/iter7500_4layers_800perlayer.log
+lein run  5000 4 1200 | tee result/iter5000_4layers_1200perlayer.log
+# 9
+lein run 90000 3  100 | tee result/iter90000_3layers_100perlayer.log
+lein run 60000 3  150 | tee result/iter60000_3layers_150perlayer.log
+lein run 45000 3  200 | tee result/iter45000_3layers_200perlayer.log
+lein run 30000 3  300 | tee result/iter30000_3layers_300perlayer.log
+lein run 20000 3  450 | tee result/iter20000_3layers_450perlayer.log
+lein run 15000 3  600 | tee result/iter15000_3layers_600perlayer.log
+lein run 10000 3  900 | tee result/iter10000_3layers_900perlayer.log
+lein run  7500 3 1200 | tee result/iter7500_3layers_1200perlayer.log
+lein run  5000 3 1500 | tee result/iter5000_3layers_1500perlayer.log
+# 18
+lein run 120000 2  100 | tee result/iter120000_2layers_100perlayer.log
+lein run  90000 2  150 | tee result/iter90000_2layers_150perlayer.log
+lein run  60000 2  200 | tee result/iter60000_2layers_200perlayer.log
+lein run  40000 2  300 | tee result/iter40000_2layers_300perlayer.log
+lein run  30000 2  400 | tee result/iter30000_2layers_400perlayer.log
+lein run  20000 2  600 | tee result/iter20000_2layers_600perlayer.log
+lein run  15000 2  800 | tee result/iter15000_2layers_800perlayer.log
+lein run  10000 2 1200 | tee result/iter10000_2layers_1200perlayer.log
+lein run   7500 2 1600 | tee result/iter7500_2layers_1600perlayer.log
+# 27
+lein run 60000 5   80 | tee result/iter60000_5layers_80perlayer.log
+lein run 40000 5  120 | tee result/iter40000_5layers_120perlayer.log
+lein run 30000 5  160 | tee result/iter30000_5layers_160perlayer.log
+lein run 20000 5  240 | tee result/iter20000_5layers_240perlayer.log
+lein run 15000 5  320 | tee result/iter15000_5layers_320perlayer.log
+lein run 10000 5  480 | tee result/iter10000_5layers_480perlayer.log
+lein run  8000 5  600 | tee result/iter8000_5layers_600perlayer.log
+lein run  6000 5  800 | tee result/iter6000_5layers_800perlayer.log
+lein run  5000 5 1000 | tee result/iter5000_5layers_1000perlayer.log
+# 36
