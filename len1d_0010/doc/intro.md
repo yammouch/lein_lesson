0f3a2492ee25bc8450a1e14064e40728d64ea8b1
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-08-09 16:30:07 +0900

    First checkin.

---
+++ len1d_0010/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to len1d_0010
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ len1d_0010/src/len1d_0010/core.clj
@@ -0,0 +1,7 @@
+(ns len1d-0010.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
