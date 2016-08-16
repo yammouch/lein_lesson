69b4aeefe1712485978cd198bc24408881f9a49d
yammouch <yammouch@gmail.com>
2016-08-16 17:27:17 +0900

    Downgrade the version of JOCL.

--- jocl_0000/project.clj
+++ jocl_0000/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.jocl/jocl "2.0.0"]]
+                 [org.jocl/jocl "0.2.0-RC00"]]
   :main ^:skip-aot jocl-0000.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
