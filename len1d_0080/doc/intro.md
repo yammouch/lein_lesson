f76c305175f56c86f0efd265c61d584024c22a78
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-09-13 05:10:42 +0900

    Copied len1d_0080 with false of minibatch for ComputationLayerBuilder.

---
+++ len1d_0080/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to len1d_0080
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ len1d_0080/project.clj
@@ -0,0 +1,25 @@
+(defproject len1d_0080 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+  ; refer to
+  ; https://github.com/deeplearning4j/deeplearning4j/issues/1819 
+                 [org.nd4j/nd4j-native "0.4.0" :classifier "windows-x86_64-openblas"]
+                 [org.nd4j/nd4j-native "0.4.0" :classifier "windows-x86_64" :scope "provided"]
+                 [org.nd4j/nd4j-native-platform "0.4.0"]
+                 ;[org.nd4j/nd4j-cuda-7.5-platform "0.4.0"]
+                 [org.deeplearning4j/deeplearning4j-nlp  "0.4.0"]
+                 [org.deeplearning4j/deeplearning4j-core "0.4.0"]
+                 [org.deeplearning4j/deeplearning4j-ui   "0.4.0"]
+                 [com.google.guava/guava "19.0"]
+                 ;[org.ng4j/nd4j-native-platform "0.4.0"]
+                 [org.nd4j/canova-nd4j-image "0.0.0.17"]
+                 [org.nd4j/canova-nd4j-codec "0.0.0.17"]
+                 [jfree/jfreechart "1.0.13"]
+                 [org.deeplearning4j/arbiter-deeplearning4j "0.0.0.8"]
+                 ]
+  :main ^:skip-aot len1d-0080.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
