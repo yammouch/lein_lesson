7a8246edd3162b3a138d40244694da17feb80c83
U-OFFICE\tyam <tyam@N34125.office.amsiag.com>
2016-11-30 17:51:38 +0900

    A function 'field1' has been added and tested.

--- len2d_0010/src/len2d_0010/field.clj
+++ len2d_0010/src/len2d_0010/field.clj
@@ -6,3 +6,12 @@
             (map (fn [stop] [start stop])
                  (range start (min fs (+ start ml)))))
           (range fs)))
+
+(defn field1 [fs start stop q dir]
+  (reduce (case dir
+            :h (fn [fld s] (assoc-in fld [q s] 1))
+            :v (fn [fld s] (assoc-in fld [s q] 1))
+            identity)
+          (reduce (fn [acc x] (vec (repeat x acc)))
+                  0 [fs fs])
+          (range start (inc stop))))
